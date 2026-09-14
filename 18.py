e85694b8e2f467c2d063a719589de04d18486130
eljuanda34 <juandavidvides72@gmail.com>
2026-09-14 11:54:05 -0500

    Add files via upload

---
+++ 18.py
@@ -0,0 +1,16 @@
+e = int(input("cual es tu edad "))
+
+if e>=18:
+    print("eres mayor de edad")
+elif e<18:
+    print("eres menor de edad")
+    
+
+n1 = int(input("digite el numero "))
+
+if n1>0:
+    print("el numero es positivo ")
+elif n1<0:
+    print("el numero es negativo ")
+else:
+    print(" el numero es cero ")
---
+++ zz.py
@@ -0,0 +1,14 @@
+g = float(input(" cual es tu temperatura en grados celsius "))
+
+z = g*1.8 + 32
+
+print( "tu temperatura en grados fahrenheit es ", z )
+
+n1 = int(input("digite el numero "))
+
+if n1>5:
+    print("el numero es mayor que 5")
+elif n1<5:
+    print("el numero es mejnor que 5")
+else:
+    print(" el numero es igual que 5")
